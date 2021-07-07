2423e3c21289511bfd55920795ed9fb6e682e4c6
AndreyMalevsky <86947286+AndreyMalevsky@users.noreply.github.com>
2021-07-07 08:02:51 +0200

    First commit

--- src/main/java/org/eho/test2/cars/Truck.java
+++ src/main/java/org/eho/test2/cars/Truck.java
@@ -3,7 +3,7 @@ package org.eho.test2.cars;
 public class Truck extends Vehicle{
     public Truck(String name, Integer speed, String color) {
         super(name, speed, color);
-        speed_limit = 80;
+        speed_limit = 80;  // Это выражение должно выполняться раньше инициализации экземпляра класса Truck //
         is_police = false;
     }
 }
